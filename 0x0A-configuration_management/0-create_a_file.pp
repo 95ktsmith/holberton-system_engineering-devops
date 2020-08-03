5d29e8586d2621818bc18892a9f6fdc9031d6a4c
95ktsmith <95ktsmith@gmail.com>
2020-08-03 23:57:48 +0000

    Puppet to uppercase in file contents.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Creates a file in /tmp
 file { 'tmp file':
     path    => '/tmp/holberton',
-    content => 'I love puppet',
+    content => 'I love Puppet',
     group   => 'www-data',
     mode    => '0744',
     owner   => 'www-data'
